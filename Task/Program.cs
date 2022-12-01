1f5afa20307ada16327999d2e5fe915bfb96d104
Marina M <muracheva2001@mail.ru>
2022-12-01 13:13:10 +0700

    Add task in C#

---
+++ Task/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] array1 = new string[7] {"123", "54", "hello", "world", "run", "program", "7"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+
+void CreateSecondArray(string[] array1, string[] array2)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+CreateSecondArray(array1, array2);
+ShowArray(array2);
